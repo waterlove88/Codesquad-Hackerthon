c15b83b2e4332f9fd96f225c76cc5bb80f6478d6
Mingyu <leemingyu05@gmail.com>
2018-08-14 21:24:40 +0900

    'init'

---
+++ src/main/java/com/developer/codesquad/ForDeveloperApplication.java
@@ -0,0 +1,12 @@
+package com.developer.codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForDeveloperApplication.class, args);
+    }
+}
